c1a647de7f908bd7682838284ce6c52b0fd6db8b
Gerald <ngjingsiang@gmail.com>
2016-08-27 10:42:42 -0700

    Add constants

---
+++ iOS Starter/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  iOS Starter
+//
+//  Created by Gerald on 27/8/16.
+//  Copyright © 2016 wenqiwu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// Only use regular and bold for both fonts
+
+// for headers only
+let cairo = "Cairo"
+
+// everything else
+let quattrocentoSans = "Quattrocento Sans"
+
+//UIFont(name: cairo, size: quattrocentoSans)
