7d0410554e25717e0a4faa97e4b1d1bd1ef0189e
Wenqi Wu <wenqiwu93@gmail.com>
2016-08-27 09:50:25 -0700

    commented out unimplemented code

--- iOS Starter/PollFormViewController.swift
+++ iOS Starter/PollFormViewController.swift
@@ -24,74 +24,74 @@ class PollFormViewController: UITableViewController {
         view.backgroundColor = UIColor.whiteColor()
         self.view = view
         
-        self.lureTableView.translatesAutoresizingMaskIntoConstraints = false
-        self.lureTableView.delegate = self
-        self.lureTableView.dataSource = self
-        self.lureTableView.estimatedRowHeight = 280
-        self.lureTableView.rowHeight = UITableViewAutomaticDimension
-        
-        self.purchaseButton.setTitle("Purchase for $70", forState: .Normal)
-        self.purchaseButton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
-        self.purchaseButton.backgroundColor = self.view.tintColor
-        self.purchaseButton.translatesAutoresizingMaskIntoConstraints = false
-        
-        self.view.addSubview(self.lureTableView)
-        self.view.addSubview(self.purchaseButton)
+//        self.lureTableView.translatesAutoresizingMaskIntoConstraints = false
+//        self.lureTableView.delegate = self
+//        self.lureTableView.dataSource = self
+//        self.lureTableView.estimatedRowHeight = 280
+//        self.lureTableView.rowHeight = UITableViewAutomaticDimension
+//        
+//        self.purchaseButton.setTitle("Purchase for $70", forState: .Normal)
+//        self.purchaseButton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+//        self.purchaseButton.backgroundColor = self.view.tintColor
+//        self.purchaseButton.translatesAutoresizingMaskIntoConstraints = false
+//        
+//        self.view.addSubview(self.lureTableView)
+//        self.view.addSubview(self.purchaseButton)
         
         setupLayoutConstraints()
     }
     
     func setupLayoutConstraints() {
-        let views = [
-            "lureTableView": self.lureTableView,
-            "purchaseButton": self.purchaseButton
-        ]
-        
-        var allConstraints = [NSLayoutConstraint]()
-        
-        let mainYConstraints = NSLayoutConstraint.constraintsWithVisualFormat(
-            "V:|[lureTableView][purchaseButton(50)]|",
-            options: [],
-            metrics: nil,
-            views: views)
-        allConstraints += mainYConstraints
-        
-        let purchaseButtonXConstraints = NSLayoutConstraint.constraintsWithVisualFormat(
-            "H:|[purchaseButton]|",
-            options: [],
-            metrics: nil,
-            views: views)
-        allConstraints += purchaseButtonXConstraints
-        
-        let lureTableViewXConstraints = NSLayoutConstraint.constraintsWithVisualFormat(
-            "H:|[lureTableView]|",
-            options: [],
-            metrics: nil,
-            views: views)
-        allConstraints += lureTableViewXConstraints
-        
-        NSLayoutConstraint.activateConstraints(allConstraints)
+//        let views = [
+//            "lureTableView": self.lureTableView,
+//            "purchaseButton": self.purchaseButton
+//        ]
+//        
+//        var allConstraints = [NSLayoutConstraint]()
+//        
+//        let mainYConstraints = NSLayoutConstraint.constraintsWithVisualFormat(
+//            "V:|[lureTableView][purchaseButton(50)]|",
+//            options: [],
+//            metrics: nil,
+//            views: views)
+//        allConstraints += mainYConstraints
+//        
+//        let purchaseButtonXConstraints = NSLayoutConstraint.constraintsWithVisualFormat(
+//            "H:|[purchaseButton]|",
+//            options: [],
+//            metrics: nil,
+//            views: views)
+//        allConstraints += purchaseButtonXConstraints
+//        
+//        let lureTableViewXConstraints = NSLayoutConstraint.constraintsWithVisualFormat(
+//            "H:|[lureTableView]|",
+//            options: [],
+//            metrics: nil,
+//            views: views)
+//        allConstraints += lureTableViewXConstraints
+//        
+//        NSLayoutConstraint.activateConstraints(allConstraints)
     }
     
     // MARK: TableView
     
-    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+    override func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 5.0
     }
     
-    func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+    override func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
         return 5.0
     }
     
-    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 2
     }
     
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
     }
     
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         var cell: UITableViewCell
         if indexPath.section == 0 {
             cell = LureDetailCell()
@@ -101,7 +101,7 @@ class PollFormViewController: UITableViewController {
         return cell
     }
     
-    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         tableView.deselectRowAtIndexPath(indexPath, animated: false)
     }
     
